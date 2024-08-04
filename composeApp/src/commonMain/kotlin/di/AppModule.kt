0929a8eb886cf7e0b96041ce7ac64ba17c18956a
Kabindra Shrestha <kabindra@vianet.com.np>
2024-08-04 17:40:40 +0545

    Updated httpclient

--- composeApp/src/commonMain/kotlin/di/AppModule.kt
+++ composeApp/src/commonMain/kotlin/di/AppModule.kt
@@ -6,12 +6,15 @@ import data.source.remote.NewsDataSource
 import domain.repository.remote.NewsRepository
 import domain.usecase.remote.GetNewsUseCase
 import io.ktor.client.HttpClient
+import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.DEFAULT
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
-import io.ktor.client.plugins.logging.SIMPLE
 import io.ktor.http.ContentType
+import io.ktor.http.contentType
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 import org.koin.compose.viewmodel.dsl.viewModel
@@ -21,15 +24,28 @@ import presentation.viewmodel.NewsViewModel
 val provideHttpClientModule = module {
     single {
         HttpClient {
+            install(HttpTimeout) {
+                socketTimeoutMillis = 60_000
+                requestTimeoutMillis = 60_000
+            }
             install(ContentNegotiation) {
                 json(
-                    json = Json { ignoreUnknownKeys = true },
+                    json = Json {
+                        prettyPrint = true
+                        isLenient = true
+                        ignoreUnknownKeys = true
+                        explicitNulls = false
+                    },
                     contentType = ContentType.Application.Json
                 )
             }
             install(Logging) {
-                logger = Logger.SIMPLE
+                logger = Logger.DEFAULT
                 level = LogLevel.ALL
+            }
+            defaultRequest {
+                url("BASE_URL")
+                contentType(ContentType.Application.Json)
             }
         }
     }
