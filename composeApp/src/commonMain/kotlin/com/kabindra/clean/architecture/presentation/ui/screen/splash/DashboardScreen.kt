35a04c45b5c657395457c9fdf759cf72d349193d
Kabindra Shrestha <kabindra@veelapp.com>
2025-11-29 14:35:58 +0545

    Worked on dashboard for carousel

--- composeApp/src/commonMain/kotlin/com/kabindra/clean/architecture/presentation/ui/screen/splash/DashboardScreen.kt
+++ composeApp/src/commonMain/kotlin/com/kabindra/clean/architecture/presentation/ui/screen/splash/DashboardScreen.kt
@@ -1,25 +1,41 @@
 package com.kabindra.clean.architecture.presentation.ui.screen.splash
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.carousel.HorizontalMultiBrowseCarousel
+import androidx.compose.material3.carousel.HorizontalUncontainedCarousel
+import androidx.compose.material3.carousel.rememberCarouselState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
-import com.kabindra.clean.architecture.presentation.ui.component.AppIcon
-import com.kabindra.clean.architecture.presentation.ui.component.TextComponent
 import com.kabindra.clean.architecture.utils.Connectivity
 import com.kabindra.clean.architecture.utils.error.GlobalErrorDialog
+import composemultiplatformcleanarchitecture.composeapp.generated.resources.Res
+import composemultiplatformcleanarchitecture.composeapp.generated.resources.eight
+import composemultiplatformcleanarchitecture.composeapp.generated.resources.five
+import composemultiplatformcleanarchitecture.composeapp.generated.resources.four
+import composemultiplatformcleanarchitecture.composeapp.generated.resources.nine
+import composemultiplatformcleanarchitecture.composeapp.generated.resources.one
+import composemultiplatformcleanarchitecture.composeapp.generated.resources.seven
+import composemultiplatformcleanarchitecture.composeapp.generated.resources.six
+import composemultiplatformcleanarchitecture.composeapp.generated.resources.ten
+import composemultiplatformcleanarchitecture.composeapp.generated.resources.three
+import composemultiplatformcleanarchitecture.composeapp.generated.resources.two
+import org.jetbrains.compose.resources.DrawableResource
+import org.jetbrains.compose.resources.painterResource
 
 @Composable
 fun DashboardScreen(
@@ -50,22 +66,83 @@ fun DashboardScreen(
         return
     }
 
-    Box(modifier = Modifier.fillMaxSize()) {
-        AppIcon(
-            modifier = Modifier
-                .width(200.dp)
-                .height(200.dp)
-                .align(Alignment.Center)
-        )
-
-        TextComponent(
-            modifier = Modifier
-                .fillMaxWidth()
-                .align(Alignment.BottomCenter)
-                .offset(y = (-75).dp),
-            text = "Dashboard Screen",
-            textAlign = TextAlign.Center
-        )
+    Column(modifier = Modifier.fillMaxSize()) {
+        HorizontalMultiBrowseCarouselExample()
+        HorizontalUncontainedCarouselExample()
     }
 
+}
+
+data class CarouselItem(
+    val id: Int,
+    val imageResId: DrawableResource,
+    val contentDescription: String
+)
+
+val carouselItems = listOf(
+    CarouselItem(1, Res.drawable.one, "One"),
+    CarouselItem(2, Res.drawable.two, "Two"),
+    CarouselItem(3, Res.drawable.three, "Three"),
+    CarouselItem(4, Res.drawable.four, "Four"),
+    CarouselItem(5, Res.drawable.five, "Five"),
+    CarouselItem(6, Res.drawable.six, "Six"),
+    CarouselItem(7, Res.drawable.seven, "Seven"),
+    CarouselItem(8, Res.drawable.eight, "Eight"),
+    CarouselItem(9, Res.drawable.nine, "Nine"),
+    CarouselItem(10, Res.drawable.ten, "Ten"),
+)
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HorizontalMultiBrowseCarouselExample() {
+    val items = remember { carouselItems }
+
+    HorizontalMultiBrowseCarousel(
+        state = rememberCarouselState { items.count() },
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(top = 16.dp, bottom = 16.dp),
+        preferredItemWidth = 186.dp,
+        itemSpacing = 8.dp,
+        contentPadding = PaddingValues(horizontal = 16.dp)
+    ) { i ->
+        val item = items[i]
+        Image(
+            modifier = Modifier
+                .height(205.dp)
+                .maskClip(MaterialTheme.shapes.extraLarge),
+            painter = painterResource(resource = item.imageResId),
+            contentDescription = item.contentDescription,
+            contentScale = ContentScale.Crop
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HorizontalUncontainedCarouselExample() {
+    val items = remember { carouselItems }
+
+    HorizontalUncontainedCarousel(
+        state = rememberCarouselState { carouselItems.count() },
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(top = 16.dp, bottom = 16.dp),
+        itemWidth = 186.dp,
+        itemSpacing = 8.dp,
+        contentPadding = PaddingValues(horizontal = 16.dp)
+    ) { i ->
+        val item = carouselItems[i]
+        Image(
+            modifier = Modifier
+                .height(205.dp)
+                .maskClip(MaterialTheme.shapes.extraLarge),
+            painter = painterResource(resource = item.imageResId),
+            contentDescription = item.contentDescription,
+            contentScale = ContentScale.Crop
+        )
+    }
 }
